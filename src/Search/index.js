e24f871d2d12408bc42896101a9153736fb83aab
Miguel Fernandez <mfernandez@financialforce.com>
2020-10-22 20:55:28 +0200

    ADD first approach to request the data

--- src/Search/index.js
+++ src/Search/index.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    const search = async () => {
+      await axios.get('https://api.spotify.com/v1/search', {
+        params: {
+          query: term,
+          offset: 0,
+          limit: 20,
+          type: 'artist,track,album',
+        },
+      });
+    };
+    search();
+  }, [term]);
 
   return (
     <>
