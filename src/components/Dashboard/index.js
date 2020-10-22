aff4305882421a76fe80d393c46ae2b93246651e
Miguel Fernandez <mfernandez@financialforce.com>
2020-10-22 22:45:51 +0200

    REVERT last commit

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -9,14 +9,3 @@ const Dashboard = () => {
   );
 };
 export default Dashboard;
-
-export const getParamValues = (url) => {
-  return url
-    .slice(1)
-    .split('&')
-    .reduce((prev, curr) => {
-      const [title, value] = curr.split('=');
-      prev[title] = value;
-      return prev;
-    }, {});
-};
