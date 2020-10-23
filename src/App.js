2e688de9d0bdbc072a6af266bd779e6c825fa93a
Miguel Fernandez <mfernandez@financialforce.com>
2020-10-23 11:13:08 +0200

    ADD new components and Routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import Redirect from './components/Redirect';
+import Header from './components/Header';
+import Home from './components/Home';
+
 class AppRouter extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="main">
-          <Switch>
-            <Route path="/" component={Dashboard} exact={true} />
-            <Route path="/redirect" component={Dashboard} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <>
+        <Header></Header>
+        <BrowserRouter>
+          <div className="main">
+            <Switch>
+              <Route path="/" component={Home} exact={true} />
+              <Route path="/redirect" component={Redirect} />
+              <Route path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </>
     );
   }
 }
