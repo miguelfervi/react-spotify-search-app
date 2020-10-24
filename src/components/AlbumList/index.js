70be75c08103a28a3ea8a0235d258df27ddadddf
Miguel Fernandez <mfernandez@financialforce.com>
2020-10-24 18:00:26 +0200

    ADD example styling

--- src/components/AlbumList/index.js
+++ src/components/AlbumList/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 const AlbumsList = ({ results }) => {
-  return <div>{console.log(results)}</div>;
+  return (
+    <div className="list-view">
+      <div class="card-bordered">
+        <h1>Title</h1>
+        <p>Kappa</p>
+      </div>
+      <div class="card-bordered">
+        <h1>Title</h1>
+        <p>Kappa</p>
+      </div>
+      <div class="card-bordered">
+        <h1>Title</h1>
+        <p>Kappa</p>
+      </div>
+      <div class="card-bordered">
+        <h1>Title</h1>
+        <p>Kappa</p>
+      </div>
+
+      {console.log(results)}
+    </div>
+  );
 };
 
 export default AlbumsList;
