d920c8df003a4a05b924aebebd3ef547a77f16b2
Miguel Fernandez <mfernandez@financialforce.com>
2020-10-26 17:41:40 +0100

    REFACTOR expiry session

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -5,13 +5,12 @@ import Search from '../Search';
 
 const Dashboard = (props) => {
   useEffect(() => {
-    const { setExpiryTime, location } = props;
+    const { location } = props;
     const loginData = location.hash;
 
     const expiryTime = new Date().getTime() + loginData.expires_in * 1000;
     localStorage.setItem('params', JSON.stringify(loginData));
     localStorage.setItem('expiry_time', expiryTime);
-    setExpiryTime(expiryTime);
     props.signIn(true);
   });
 
