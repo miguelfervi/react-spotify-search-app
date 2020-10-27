8ffde0f3d639ed7bec2b3a977c714dc6f10bbf08
Miguel Fernandez <mfernandez@financialforce.com>
2020-10-27 12:45:47 +0100

    ADD unit tests and README

---
+++ src/components/Filter/__tests__/Filter.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Filter from '../index';
+
+describe('Filter', () => {
+  test('renders Filter component', () => {
+    render(<Filter />);
+  });
+});
