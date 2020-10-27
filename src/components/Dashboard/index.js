93b0f460317d3cbec8316b1e9ef537fc25cd31bd
Miguel Fernandez <mfernandez@financialforce.com>
2020-10-27 12:37:15 +0100

    MINOR imrpovments

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -4,15 +4,14 @@ import { signIn } from '../../actions';
 import Search from '../Search';
 
 const Dashboard = (props) => {
+  const { location, signIn } = props;
+  const loginData = location.hash;
   useEffect(() => {
-    const { location } = props;
-    const loginData = location.hash;
-
     const expiryTime = new Date().getTime() + loginData.expires_in * 1000;
     localStorage.setItem('params', JSON.stringify(loginData));
     localStorage.setItem('expiry_time', expiryTime);
-    props.signIn(true);
-  });
+    signIn(true);
+  }, [signIn, loginData]);
 
   return (
     <>
